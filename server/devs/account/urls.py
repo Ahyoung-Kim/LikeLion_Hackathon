22a87e2f4fba007b57d5208e743a5bf64408e654
sen <64001680+dahyen0o@users.noreply.github.com>
2022-08-02 21:15:49 +0900

    add login

--- server/devs/account/urls.py
+++ server/devs/account/urls.py
@@ -1,8 +1,9 @@
 from django.urls import URLPattern, path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import Test, UserView
+from .views import Test, UserLoginView, UserView
 
 urlpatterns = [
   path('test/', Test.as_view()),
   path('signup/', UserView.as_view()),
+  path('login/', UserLoginView.as_view()),
 ]
