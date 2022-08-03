183ee762095a282d4e190b31e6e073bbfa011b84
Hanrabong <48653304+andychyou@users.noreply.github.com>
2022-08-03 10:19:48 +0900

    app.js new links

--- client/devs/src/App.js
+++ client/devs/src/App.js
@@ -6,7 +6,9 @@ import { Route, Routes } from 'react-router-dom';
 // Componenets
 import LandingPage from './components/landing/LandingPage';
 import LoginPage from './components/login/LoginPage';
-import ProfilePage from './components/profile/ProfilePage';
+import SearchPage from './components/search/SearchPage';
+import ProfilePage3 from './components/profile/profile3/ProfilePage3';
+import MainPage from './components/main/MainPage';
 
 const App = () => {
   return (
@@ -16,11 +18,18 @@ const App = () => {
       <Routes>
         {/* Landing Page */}
         <Route path="/landing" element={<LandingPage />} />
+        
         {/* Login */}
         <Route path="/login" element={<LoginPage />} />
 
-        {/* Profile */}
-        <Route path="/profile" element={<ProfilePage />} />
+        {/* Search Page */}
+        <Route path="/search" element={<SearchPage />} />
+
+        {/*Profile Page 3*/}
+        <Route path="/profile3" element={<ProfilePage3 />} />
+
+        {/*Main Page*/}
+        <Route path="/mainpage" element={<MainPage/>} />
 
       </Routes>
     </div>
