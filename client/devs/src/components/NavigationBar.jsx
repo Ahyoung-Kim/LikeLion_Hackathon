7f7bfc89f2334b99050ab954a8fc77d3cc219443
Hanrabong <48653304+andychyou@users.noreply.github.com>
2022-08-03 10:12:43 +0900

    Add files via upload

---
+++ client/devs/src/components/NavigationBar.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Container, Form, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faMagnifyingGlass, faHouse, faBell, faGear} from "@fortawesome/free-solid-svg-icons";
+import './navigationbar.css';
+
+
+const NavigationBar = () => {
+  return (
+    <>
+      <Navbar className='navbar' styledbg="light" expand="lg">
+      <Container fluid>
+        <div className='nav-logo'>
+          <span className="maintitle" href="#">뎁스</span>
+          <span className="subtitle" href="#">devStory</span>
+        </div>
+  
+
+        <Nav
+          className="me-auto my-2 my-lg-0"
+          style={{ maxHeight: '100spanx' }}
+          navbarScroll
+        >
+          
+          <Form  style={{marginLeft:'80px'}}className="d-flex">
+          <Form.Control 
+            type="search"
+            placeholder="Search"
+            className="nav-searchbar me-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-success">
+            <FontAwesomeIcon icon={faMagnifyingGlass} size='lg'/>
+          </Button>
+          </Form>
+        
+    
+        </Nav>
+          
+        <span class="nav-icon material-symbols-outlined">
+        notifications
+        </span>
+        <span class="nav-icon material-symbols-outlined">
+        home
+        </span>
+        <span class="nav-icon material-symbols-outlined">
+        settings
+        </span>
+        {/* <FontAwesomeIcon className='nav-icon' icon={faHouse} />
+        <FontAwesomeIcon className='nav-icon' icon={faBell} />
+        <FontAwesomeIcon className='nav-icon' icon={faGear} /> */}
+        <img className="profile-pic" style={{width:'26px', marginLeft:'24px'}}src={require('./search/profile-img.png')}></img>
+
+      </Container>
+    </Navbar>
+    </>
+  );
+
+};
+
+export default React.memo(NavigationBar);
