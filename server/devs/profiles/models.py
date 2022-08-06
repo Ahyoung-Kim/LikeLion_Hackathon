b75cfac4f8f35f6bdb6e61af65e63f75397f5a21
taeho0888 <taeho0888@naver.com>
2022-08-06 16:42:31 +0900

    add image, remove comment

--- server/devs/profiles/models.py
+++ server/devs/profiles/models.py
@@ -41,26 +41,6 @@ class UserSkills(models.Model):  # 기술스택
     skill_name = models.CharField(max_length=100, null=True)
 
 
-# class UserSkillsLan(models.Model):  # 기술스택 - Programming Language
-#     id = models.BigAutoField(primary_key=True)
-#     post = models.ForeignKey(
-#         "UserSkills",
-#         related_name="post_anguage",
-#         on_delete=models.CASCADE,
-#         db_column="post_anguage")
-#     language = models.CharField(max_length=30)
-
-
-# class UserSkillsFram(models.Model):  # 기술스택 - Framework / Library
-#     id = models.BigAutoField(primary_key=True)
-#     post = models.ForeignKey(
-#         "UserSkills",
-#         related_name="post_framework",
-#         on_delete=models.CASCADE,
-#         db_column="post_framework")
-#     framework = models.CharField(max_length=30)
-
-
 class UserStudy(models.Model):  # 개인공부
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
@@ -69,26 +49,6 @@ class UserStudy(models.Model):  # 개인공부
     updated_at = models.DateTimeField(auto_now=True)
     study_type = models.CharField(max_length=100, null=True)
     study_name = models.CharField(max_length=100, null=True)
-
-
-# class UserStudyCS(models.Model):  # 개인공부 - CS
-#     id = models.BigAutoField(primary_key=True)
-#     post = models.ForeignKey(
-#         "UserStudy",
-#         related_name="CS",
-#         on_delete=models.CASCADE,
-#         db_column="post_cs")
-#     CS = models.CharField(max_length=30)
-
-
-# class UserStudyPS(models.Model):  # 개인공부 - PS
-#     id = models.BigAutoField(primary_key=True)
-#     post = models.ForeignKey(
-#         "UserStudy",
-#         related_name="PS",
-#         on_delete=models.CASCADE,
-#         db_column="post_ps")
-#     PS = models.CharField(max_length=30)
 
 
 class UserCert(models.Model):  # 자격증
--- server/devs/profiles/urls.py
+++ server/devs/profiles/urls.py
@@ -1,17 +1,4 @@
-# from django.urls import URLPattern, path
-# from rest_framework.urlpatterns import format_suffix_patterns
-# from .views import Test, UserLoginView, UserView
-# from .views import detail
-
-# urlpatterns = [
-#     path('test/', Test.as_view()),
-#     path('signup/', UserView.as_view()),
-#     path('login/', UserLoginView.as_view()),
-#     path('<int:pk>/', views.detail, name='detail')
-# ]
-
 from django.urls import path, include
-#from .views import UserDetailsDetail, UserDetailsView
 from . import views
 from rest_framework.routers import DefaultRouter
 
@@ -19,11 +6,7 @@ router = DefaultRouter()
 router.register('details', views.UserDetailsViewSet)
 router.register('ongoing', views.UserOngoingViewSet)
 router.register('skills', views.UserSkillsViewSet)
-# router.register('skills-lan', views.UserSkillsLanViewSet)
-# router.register('skills-fram', views.UserSkillsFramViewSet)
 router.register('study', views.UserStudyViewSet)
-# router.register('studyCS', views.UserStudyCSViewSet)
-# router.register('studyPS', views.UserStudyPSViewSet)
 router.register('cert', views.UserCertViewSet)
 router.register('career', views.UserCareerViewSet)
 router.register('follow',views.FollowViewSet)
