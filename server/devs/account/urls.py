87381e29efcc778505695a424808c164fec4340b
taeho0888 <taeho0888@naver.com>
2022-08-14 17:30:25 +0900

    add userinfo

--- server/devs/account/urls.py
+++ server/devs/account/urls.py
@@ -1,10 +1,15 @@
 from django.urls import URLPattern, path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import Test, UserLoginView, UserView
+from .views import Test, UserLoginView, UserView, UserInfoViewSet
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
+router = DefaultRouter()
+router.register('userinfo', UserInfoViewSet)
+
+urlpatterns = router.get_urls()
+
+urlpatterns += [
     path('test/', Test.as_view()),
     path('signup/', UserView.as_view()),
     path('login/', UserLoginView.as_view()),
-    # path('<int:pk>/follow/', views.follow, name='follow'),
 ]
