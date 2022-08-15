ad255315e09e5b5013b146a3fc3ab294ae0d300f
Hanrabong <48653304+andychyou@users.noreply.github.com>
2022-08-16 01:17:27 +0900

    fixed devs-urls.py

--- server/devs/devs/urls.py
+++ server/devs/devs/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-from django.views.static import serve
-from django.conf import settings	
+
+from django.conf import settings   
 from django.conf.urls.static import static
 
 
@@ -11,8 +11,8 @@ urlpatterns = [
     #path('test/', include('account.urls'))
     path('account/', include('account.urls')),
     path('profiles/', include('profiles.urls')),
-    re_path('.*', TemplateView.as_view(template_name="index.html")),
+    #re_path('.*', TemplateView.as_view(template_name="index.html")),
     
 ]
-
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += re_path('.*', TemplateView.as_view(template_name="index.html")),
